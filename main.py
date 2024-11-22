a3582a12a75fabe2449fa72d94ef39e1297c6859
alexis-gauluet <alexis.gauluet@edu.esiee.fr>
2024-11-22 13:40:04 +0000

    exercice dans la continuité des suites de Siracuse, cette fois on stock les differentes valeurs dans des listes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+"""code en continuité de l'exercice Suites de Siracuse, 
+pour stocker les valeurs de la suite"""
 #### Fonctions secondaires
 
 
@@ -6,6 +8,7 @@ from plotly.graph_objects import Scatter, Figure
 
 ### NE PAS MODIFIER ###
 def syr_plot(lsyr):
+    """afficher le shema de la suite lsyr"""
     title = "Syracuse" + " (n = " + str(lsyr[0]) + " )"
     fig = Figure({  'layout':   { 'title': {'text': title},
                                 'xaxis': {'title': {'text':"x"}},
@@ -14,12 +17,13 @@ def syr_plot(lsyr):
                 }
     )
 
-    x = [ i for i in range(len(lsyr)) ]
+    x = list(range(len(lsyr))) #[ i for i in range(len(lsyr)) ] erreur dans la notation,
+    #demande d'ajouter list(range(...)).
+    # Quand on modifie, demande d'ajouter encore un list(...) etc
     t = Scatter(x=x, y=lsyr, mode="lines+markers", marker_color = "blue")
     fig.add_trace(t)
     fig.show()
     # fig.write_html('fig.html', include_plotlyjs='cdn')
-    return None
 #######################
 
 def syracuse_l(n):
@@ -32,10 +36,21 @@ def syracuse_l(n):
         list: la suite de Syracuse de source n
     """
 
-    # votre code ici 
-    l = [ ]
-    return l
+    # votre code ici
 
+    l = [ ]
+    if n==1:
+        return 1
+    i=0
+    while n!=1:
+        l.append(n)
+        if n%2==0:
+            n=n/2
+        else:
+            n=n*3+1
+        i+=1
+    l.append(1)
+    return l
 def temps_de_vol(l):
     """Retourne le temps de vol d'une suite de Syracuse
 
@@ -45,11 +60,9 @@ def temps_de_vol(l):
     Returns:
         int: le temps de vol
     """
-    
-    # votre code ici
+    #votre code ici
 
-    n = 0
-    return n
+    return len(l)
 
 def temps_de_vol_en_altitude(l):
     """Retourne le temps de vol en altitude d'une suite de Syracuse
@@ -62,10 +75,10 @@ def temps_de_vol_en_altitude(l):
     """
 
     # votre code ici
-
-    n = 0
-    return n
-
+    tva=1
+    while l[0]<l[tva] :
+        tva+=1
+    return tva
 
 def altitude_maximale(l):
     """retourne l'altitude maximale d'une suite de Syracuse
@@ -76,24 +89,19 @@ def altitude_maximale(l):
     Returns:
         int: l'altitude maximale
     """
-    
     # votre code ici
-    
-    n = 0
-    return n
 
+    return max(l)
 
 #### Fonction principale
 
-
 def main():
-
+    """fonction principale"""
     # vos appels à la fonction secondaire ici
     lsyr = syracuse_l(15)
     syr_plot(lsyr)
     print(temps_de_vol(lsyr))
     print(temps_de_vol_en_altitude(lsyr))
-    print(altitude_maximale(lsyr))
 
 
 if __name__ == "__main__":
